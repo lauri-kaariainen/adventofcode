f2aecbe0d5f81de7ddd5949ae6b9b1ca45d7e596
Lauri Kääriäinen <lauri.v.kaariainen@gmail.com>
2019-12-01 13:08:22 +0200

    add permutations func

--- help.js
+++ help.js
@@ -135,3 +135,21 @@ const reduceMultiply = (acc, next) =>
 
 const reduceFlatten = (acc, next) =>
   acc.concat(next);
+
+
+const permutations = xs => {
+  let ret = [];
+
+  for (let i = 0; i < xs.length; i = i + 1) {
+    let rest = permutations(xs.slice(0, i).concat(xs.slice(i + 1)));
+
+    if (!rest.length) {
+      ret.push([xs[i]])
+    } else {
+      for (let j = 0; j < rest.length; j = j + 1) {
+        ret.push([xs[i]].concat(rest[j]))
+      }
+    }
+  }
+  return ret;
+}
