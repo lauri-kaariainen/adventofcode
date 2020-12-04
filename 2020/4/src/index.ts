5a6d839125000efc1c36ef9f95a99bab24ca4073
Lauri Kääriäinen <lvk84@hotmail.com>
2020-12-04 17:50:40 +0200

    2020 4 B

--- 2020/4/src/index.ts
+++ 2020/4/src/index.ts
@@ -29,7 +29,7 @@ type Passport = {
 
 type PassportKey = keyof Passport;
 
-const checkIfValid = (str: string, minimumValidKeys: string[]) => {
+const checkIfValid = (str: string, minimumValidKeys: string[], runStrictTest: boolean = false) => {
     const passportObj: Passport = str
         .split(/\s/)
         .reduce((curr: Passport, next: string): Passport =>
@@ -43,14 +43,28 @@ const checkIfValid = (str: string, minimumValidKeys: string[]) => {
             ecl: "",
             pid: ""
         })
+
     return minimumValidKeys
         .findIndex((key: string) =>
             (!Object.keys(passportObj).includes(key) ||
                 !(passportObj[<PassportKey>key] !== "")))
         === -1
-    //B:
-    // &&
-    // betweenOrEqual(passportObj.byr, 1920, 2002)
+        //B:
+        &&
+        !runStrictTest ? true :
+        true &&
+        betweenOrEqual(parseInt(passportObj.byr), 1920, 2002) &&
+        betweenOrEqual(parseInt(passportObj.iyr), 2010, 2020) &&
+        betweenOrEqual(parseInt(passportObj.eyr), 2020, 2030) &&
+        passportObj.hgt.match(/^\d+[a-z]{2}$/i) &&
+        (passportObj.hgt.toLowerCase().match(/[a-z]+/)![0] === "cm" &&
+            betweenOrEqual(parseInt(passportObj.hgt.match(/\d+/)![0]), 150, 193)
+            ||
+            (passportObj.hgt.toLowerCase().match(/[a-z]+/)![0] === "in" &&
+                betweenOrEqual(parseInt(passportObj.hgt.match(/\d+/)![0]), 59, 76))) &&
+        passportObj.hcl.match(/^#\w{6}$/i) &&
+        passportObj.ecl.match(/^(amb|blu|brn|gry|grn|hzl|oth)$/) &&
+        passportObj.pid.match(/^\d{9}$/)
 
 
 }
@@ -60,7 +74,7 @@ const checkIfValid = (str: string, minimumValidKeys: string[]) => {
 
 log("test a:",
 
-    testInputArr.filter((line: string) => checkIfValid(line, VALIDSFORA))
+    testInputArr.filter((line: string) => checkIfValid(line, VALIDSFORA)).length
 
 )
 
@@ -71,12 +85,13 @@ log("a:",
 
 
 log("test b:",
-
+    getValidTestInput().filter((line: string) => checkIfValid(line, VALIDSFORA, true)).length,
+    getInvalidTestInput().filter((line: string) => checkIfValid(line, VALIDSFORA, true)).length
 
 )
 
 log("b:",
-
+    inputArr.filter((line: string) => checkIfValid(line, VALIDSFORA, true)).length
 )
 
 
@@ -106,6 +121,39 @@ function getTESTInput(): string[] {
     
     hcl:#cfa07d eyr:2025 pid:166559648
     iyr:2011 ecl:brn hgt:59in`)
+}
+
+function getValidTestInput(): string[] {
+    return handleInputStr(`pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+    hcl:#623a2f
+    
+    eyr:2029 ecl:blu cid:129 byr:1989
+    iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+    
+    hcl:#888785
+    hgt:164cm byr:2001 iyr:2015 cid:88
+    pid:545766238 ecl:hzl
+    eyr:2022
+    
+    iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719`)
+
+}
+
+function getInvalidTestInput(): string[] {
+    return handleInputStr(`eyr:1972 cid:100
+    hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+    
+    iyr:2019
+    hcl:#602927 eyr:1967 hgt:170cm
+    ecl:grn pid:012533040 byr:1946
+    
+    hcl:dab227 iyr:2012
+    ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+    
+    hgt:59cm ecl:zzz
+    eyr:2038 hcl:74454a iyr:2023
+    pid:3556412378 byr:2007`)
+
 }
 
 function getInput(): string[] {
