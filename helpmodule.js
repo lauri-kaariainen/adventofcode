22f50efcb1352e61d0d29bbf5bce18dcd4a56f9d
Lauri Kääriäinen <lvk84@hotmail.com>
2020-12-14 02:58:45 +0200

    add gcm and lcm to helpmodule

--- helpmodule.js
+++ helpmodule.js
@@ -172,6 +172,32 @@ const returnUniqueAmountsAsObj = array =>
 const betweenOrEqual = (number, min, max) =>
   number >= min && number <= max
 
+function gcd2(a, b) {
+  // Greatest common divisor of 2 integers
+  if (!b) return b === 0 ? a : NaN;
+  return gcd2(b, a % b);
+}
+function gcd(array) {
+  // Greatest common divisor of a list of integers
+  var n = 0;
+  for (var i = 0; i < array.length; ++i)
+    n = gcd2(array[i], n);
+  return n;
+}
+function lcm2(a, b) {
+  // Least common multiple of 2 integers
+  return a * b / gcd2(a, b);
+}
+function lcm(array) {
+  // Least common multiple of a list of integers
+  var n = 1;
+  for (var i = 0; i < array.length; ++i)
+    n = lcm2(array[i], n);
+  return n;
+}
+
+
+
 export {
   find4,
   get8,
@@ -192,5 +218,7 @@ export {
   reduceFlatten,
   permutations,
   returnUniqueAmountsAsObj,
-  betweenOrEqual
+  betweenOrEqual,
+  lcm,
+  gcd
 }
